26fe863f5fb09ab9e977b43d95c760ad3608d9ae
Vladimir <guiderwg@gmail.com>
2020-10-05 17:42:44 +0400

    Added slider

--- resources/js/jQueryScripts.js
+++ resources/js/jQueryScripts.js
@@ -19,5 +19,46 @@ export default $(document).ready(() => {
     );
     /*---*/
 
+    /* Mobile menu */
+    let menuLink = $('.mobile-menu-btn');
+    $('#mobile-nav').hcOffcanvasNav({
+        width: '55%',
+        disableAt: 992,
+        customToggle: menuLink,
+        labelClose: $('<img src="/images/logo.svg" alt="" class="logo__img logo__img_mobile img-fluid">'),
+        labelBack: 'Назад',
+
+    });
+    /* END Mobile menu */
+
+    if($.fn.slick) {
+        $('.main-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            asNavFor: '.slider-nav',
+            speed: 800,
+            fade: true,
+        });
+
+        $('.slider-nav').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true,
+            asNavFor: '.main-slider',
+            variableWidth: true,
+            arrows: true,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 400000,
+            speed: 300,
+            centerMode: true,
+            focusOnSelect: true,
+            prevArrow:"<img class='a-left control-c prev slick-prev' src='/images/icons/prev.svg'>",
+            nextArrow:"<img class='a-right control-c next slick-next' src='/images/icons/next.svg'>"
+        });
+
+    }
 
 });
